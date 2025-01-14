de1e15a043766568ef9755f123db5d1b6bfeb920
RafalKielbasa <kielbasarafal92@gmail.com>
2025-01-14 15:57:07 +0100

    middleware

--- server2.js
+++ server2.js
@@ -3,29 +3,55 @@ import { users } from './mockData.js'
 
 const PORT = process.env.PORT || 3000
 
-const server = createServer((req, res) => {
-  const requestUrl = req.url
-  const requestMethod = req.method
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+}
 
-  if (requestMethod === 'GET') {
-    if (requestUrl === '/api/users') {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(users))
-    } else if (requestUrl.match(/\/api\/users\/([0-9]+)/)) {
-      const id = parseInt(requestUrl.split('/')[3])
-      const user = users.find((user) => user.id === id)
-      if (user) {
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(user))
-      } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify({ message: 'User not found' }))
-      }
-    } else {
-      res.writeHead(404, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: 'Route not found' }))
-    }
+const jsonMiddleware = (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json')
+  next()
+}
+
+const allUsersHandler = (req, res) => {
+  res.statusCode = 200
+  res.end(JSON.stringify(users))
+}
+
+const userByIdHandler = (req, res) => {
+  const id = parseInt(req.url.split('/')[3])
+  const user = users.find((user) => user.id === id)
+  if (user) {
+    res.statusCode = 200
+    res.end(JSON.stringify(user))
+  } else {
+    res.statusCode = 200
+    res.end(JSON.stringify({ message: 'User not found' }))
   }
+}
+
+const notFoundHandler = (req, res) => {
+  res.statusCode = 404
+  res.end(JSON.stringify({ message: 'Route not found' }))
+}
+
+const server = createServer((req, res) => {
+  jsonMiddleware(req, res, () => {
+    logger(req, res, () => {
+      const requestUrl = req.url
+      const requestMethod = req.method
+
+      if (requestMethod === 'GET') {
+        if (requestUrl === '/api/users') {
+          allUsersHandler(req, res)
+        } else if (requestUrl.match(/\/api\/users\/([0-9]+)/)) {
+          userByIdHandler(req, res)
+        } else {
+          notFoundHandler(req, res)
+        }
+      }
+    })
+  })
 })
 
 server.listen(PORT, () => {
